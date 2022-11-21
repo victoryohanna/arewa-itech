486ba4b000db517bed14b3a544893ae773dc36d7
Victor Yohanna <victoryohanna@gmail.com>
2022-11-21 12:00:41 +0100

    creates geolocation map for contact.jsx component

--- src/components/contact/map.jsx
+++ src/components/contact/map.jsx
@@ -1,11 +1,19 @@
+import "./contact.css";
 
-
-const Geolocator = ()=>{
-    return(
-        <div>
-            <h1>Map</h1>
-        </div>
-    )
-}
+const Geolocator = () => {
+  return (
+    <div className="map_container">
+      <h3>Our Location</h3>
+      <iframe
+        className="map_canvas"
+        src="https://maps.google.com/maps?q=Banex%20Plaza%20Wuse&t=&z=13&ie=UTF8&iwloc=&output=embed"
+        frameborder="0"
+        scrolling="no"
+        marginheight="0"
+        marginwidth="0"
+      ></iframe>
+    </div>
+  );
+};
 
 export default Geolocator;
